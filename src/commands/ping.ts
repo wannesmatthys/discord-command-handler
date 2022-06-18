5861230541a8e06a04875ec44c81a04417c3969c
Wannes Matthys <wannesmatthys@gmail.com>
2022-06-18 19:33:58 +0200

    Simplify command files

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,12 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
 
-const data = new SlashCommandBuilder()
+export const data = new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!");
 
-const execute = async (interaction: CommandInteraction): Promise<void> => {
+export const execute = async (
+    interaction: CommandInteraction
+): Promise<void> => {
     await interaction.reply("Pong!");
 };
-
-export { data, execute };
